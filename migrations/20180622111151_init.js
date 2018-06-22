d8f2b4289360a09f8eb9d4df1add217c43eb4433
Edward <edpantzar@gmail.com>
2018-06-22 11:22:32 -0500

    Added first migration file

--- migrations/20180622111151_init.js
+++ migrations/20180622111151_init.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('assassins', (table) => {
+    table.increments();
+    table.string('name');
+    table.string('weapon');
+    table.integer('min_price');
+    table.float('rating');
+    table.integer('kills');
+    table.integer('open_contracts');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('assassins');
 };
