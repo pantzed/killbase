af7a5ca802e1e6a691b0e04608265bc0a3850145
Edward <edpantzar@gmail.com>
2018-06-25 09:22:29 -0500

    Added gitignore file

--- kb.js
+++ kb.js
@@ -1,10 +1,49 @@
 'use strict'
 
-const env = 'development';
-const config = require('/knexfile.js')[env];
-const knex = require('knex')(config);
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express');
+
+const env = 'development';
+const knexConfigPath = path.join(__dirname, 'knexfile.js');
+const config = require(knexConfigPath)[env];
+const knex = require('knex')(config);
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+app.disable('x-powered-by');
+app.use(bodyParser.json());
+
+
+app.get('/assassins', (req, res) => {
+  console.log(req.body);
+  let opp = req.body.opp;
+  console.log(opp);
+
+  if (req.body.id !== undefined) {
+      res.send(x);
+  }
+  else {
+    knex('assassins').then((x) => {
+      res.send(x);
+    });
+  }
+});
+
+app.get('/contracts', (req, res) => {
+  knex('contracts').then((x) => {
+    res.send(x)
+  });
+});
+
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+
+app.listen(port, function() {
+  console.log('Listening on port', port);
+});
+
+module.exports = app;
